49430dc2b497b2e3fa2afa4e7b1f277908cca421
sso-hyeon <ashash1994@naver.com>
2021-12-28 11:07:12 +0900

    drag 수정

--- drag/js/createBox.js
+++ drag/js/createBox.js
@@ -6,28 +6,27 @@ let startY;
 let box;
 
 document.addEventListener("click", e => {
-    if (e.target.classList.contains("closeBtn")) {
+    if (e.target.matches(".closeBtn")) {
         e.target.parentNode.remove();
+        return (createMode = false);
+    } else {
+        !createMode ? createStart(e) : createEnd(e);
     }
 });
-
-document.addEventListener("mousedown", mouseClick);
 document.addEventListener("mousemove", createBox);
-document.addEventListener("mouseup", mouseDrop);
 
-function mouseClick(e) {
+function createStart(e) {
     createMode = true;
     box = document.createElement("div");
-    // console.log(startX, startY);
+    box.classList = "box";
     startX = e.x;
     startY = e.y;
 }
 function createBox(e) {
     if (!createMode) return;
-    box.classList = "box";
 
-    if (e.clientX - startX < 0) startX = e.clientX;
-    if (e.clientY - startY < 0) startY = e.clientY;
+    // if (e.clientX - startX < 0) startX = e.clientX;
+    // if (e.clientY - startY < 0) startY = e.clientY;
 
     box.style.top = startY + "px";
     box.style.left = startX + "px";
@@ -36,10 +35,20 @@ function createBox(e) {
 
     body.append(box);
 }
-function mouseDrop() {
+function createEnd() {
     createMode = false;
-    let deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "x";
-    deleteBtn.classList = "closeBtn";
+    let deleteBtn = document.createElement("i");
+    deleteBtn.classList = "fas fa-times closeBtn";
+    deleteBtn.addEventListener("click", e => {
+        createMode = true;
+    });
+    let text = prompt("내용을 작성해주세요.");
+    box.innerText = text;
+    box.style.background = "rgb(" + randomNum(0, 255) + "," + randomNum(0, 255) + "," + randomNum(0, 255) + ")";
+    box.style.border = 0;
     box.append(deleteBtn);
+}
+
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
